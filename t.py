5630daf8e7ea88c86fe7a3f45804ee194f0829c8
Tanmay04032002 <tanmayaggarwal403@gmail.com>
2026-03-30 19:52:21 +0530

    Update t.py

--- t.py
+++ t.py
@@ -6,7 +6,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 import threading
 
 # ---------------- CONFIG ----------------
-START_ID = 109450
+START_ID = 112380
 END_ID = 200000
 BASE_URL = "https://www.taxsutra.com/download/attachment-conclusion/{}"
 DOWNLOAD_DIR = "t_downloads"          # ← CHANGED from "downloads"
